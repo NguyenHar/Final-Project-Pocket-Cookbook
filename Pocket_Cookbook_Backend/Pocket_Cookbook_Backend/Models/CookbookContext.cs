dceabb1d95f4e4784476fb6233f2a2e51ff31f35
harry <harry.nguyen.2016@gmail.com>
2023-06-14 11:49:13 -0400

    linked to database

---
+++ Pocket_Cookbook_Backend/Pocket_Cookbook_Backend/Models/CookbookContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pocket_Cookbook_Backend.Models
+{
+    public class CookbookContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public CookbookContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Cookbook;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+    }
+}
