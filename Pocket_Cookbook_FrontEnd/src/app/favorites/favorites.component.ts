d1149f11d0b37577a9ee570fa2ebe16b750352ea
harry <harry.nguyen.2016@gmail.com>
2023-06-16 11:39:46 -0400

    added page redirects

--- Pocket_Cookbook_FrontEnd/src/app/favorites/favorites.component.ts
+++ Pocket_Cookbook_FrontEnd/src/app/favorites/favorites.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MealService } from '../meal.service';
+import { Result } from '../meal';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent {
+  favoritedMeals:Result[] = [];
 
+  constructor(private mealService:MealService, private router:Router) {
+  }
+  // Update the displayed list whenver the page is modified
+  ngOnInit() {
+    this.favoritedMeals = this.mealService.favoritedMeals;
+  }
+
+  // Stores the selected meal result in meal.service.ts as selectedMeal
+  selectRecipe(r:Result):void{
+    this.mealService.selectedMeal = r;
+    this.router.navigate(['recipe']);
+  }
+
+  // Removes the favorited meal from meal.service.ts's favoritedMeals
+  removeFavorite(index:number):void {
+    this.mealService.favoritedMeals.splice(index, 1);
+  }
 }
